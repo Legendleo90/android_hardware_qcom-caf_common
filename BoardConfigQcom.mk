6f64ee5f4a5ed9b6b960400947cc33b6dd838798
duckyduckG <duckyduckg65@gmail.com>
2025-10-27 13:09:02 +0500

    qcom: enable no_camera_custom_format for UM 4.14 for kernel 4.19

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -286,10 +286,14 @@ ifneq ($(filter $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(U
     SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 
-# Enable Supported halls on UM 4.9 platforms with kernel 4.19 upgrade
 ifeq ($(TARGET_KERNEL_VERSION),4.19)
-ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+# Enable no_camera_custom_format on UM 4.9 and 4.14 platforms with kernel 4.19 upgrade
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     SOONG_CONFIG_qtidisplay_target_no_camera_custom_format := true
+endif
+
+# Enable Supported halls on UM 4.9 platforms with kernel 4.19 upgrade
+ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 endif
